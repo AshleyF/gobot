8a0cc738d3ca4fcd2943dbbe2fcdfa03398f2f10
AshleyF <ashley.feniello@live.com>
2016-08-26 18:55:48 -0700

    Beginning simplistic GTP client

---
+++ gtp/gtp/GtpClient.fs
@@ -0,0 +1,45 @@
+﻿module GtpClient
+
+open System.Net.Sockets
+open System.Text
+open System.Threading
+
+type GtpClient(host, port) =
+    let client = new TcpClient(host, port)
+    let stream = client.GetStream()
+    let buffer = Array.create 1024 0uy
+    let read () =
+        let count = stream.Read(buffer, 0, buffer.Length)
+        let str = Encoding.ASCII.GetString(buffer, 0, count)
+        str
+    let write (cmd: string) =
+        printfn "\nCOMMAND: %s" cmd
+        let count = Encoding.ASCII.GetBytes(cmd, 0, cmd.Length, buffer, 0)
+        stream.Write(buffer, 0, count)
+        stream.Flush()
+        Thread.Sleep 100 // TODO: remove
+    let coord x y = sprintf "%c%i" "ABCDEFGHJKLMNOPQRST".[x] (y + 1)
+    let readSuccess () =
+        let result = read ()
+        printfn "RESULT: %s" result
+        //if result.[0] <> '=' then sprintf "GTP error: %s" result |> failwith
+    let simpleCommand cmd =
+        write cmd
+        readSuccess ()
+
+    member this.BoardSize(size) = sprintf "boardsize %i\n" size |> simpleCommand
+    member this.Komi(komi) = sprintf "komi %f\n" komi |> simpleCommand
+    member this.Level(level) = sprintf "level %i\n" level |> simpleCommand
+    member this.ClearBoard() = simpleCommand "clear_board\n"
+    member this.Play(color, x, y) = sprintf "play %c %s\n" color (coord x y) |> simpleCommand
+    member this.Board() =
+        // Thread.Sleep 100
+        // read () |> printfn "EXTRA: %s"
+        write "showboard\n"
+        Thread.Sleep 100
+        read () |> printfn "BOARD: %s"
+    member this.Move(color) =
+        sprintf "genmove %c\n" color |> simpleCommand
+    member this.Quit() =
+        simpleCommand "quit\n"
+        client.Close()
---
+++ gtp/gtp/Main.fs
@@ -0,0 +1,33 @@
+﻿module Main
+
+open GtpClient
+
+type Stone = Black | White
+type Point = int * int
+
+type Board() =
+    let mutable board = Map.empty<Point, Stone>
+    member this.Add(point, stone) = board <- Map.add point stone board
+    member this.Remove(point) = board <- Map.remove point board
+    member this.Stones() = Map.toList board
+
+let sync (client: GtpClient) (board: Board) =
+    client.ClearBoard()
+    board.Stones() |> List.iter (fun ((x, y), s) -> client.Play((match s with Black -> 'B' | White -> 'W'), x, y))
+
+let client = new GtpClient("localhost", 1234)
+client.BoardSize 9
+client.Komi 5.5
+client.Level 1
+
+let board = new Board()
+board.Add((0, 0), Black)
+board.Add((1, 1), White)
+
+sync client board
+
+client.Board()
+client.Move('W')
+client.Move('B')
+client.Board()
+client.Quit()
