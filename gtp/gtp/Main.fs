66cf7fe8d88346f22147a713e01836562b81ed02
AshleyF <ashley.feniello@live.com>
2016-08-26 19:08:26 -0700

    Testing winform on Ubuntu

--- gtp/gtp/Main.fs
+++ gtp/gtp/Main.fs
@@ -15,6 +15,7 @@ let sync (client: GtpClient) (board: Board) =
     client.ClearBoard()
     board.Stones() |> List.iter (fun ((x, y), s) -> client.Play((match s with Black -> 'B' | White -> 'W'), x, y))
 
+// gnugo.exe --quiet --never-resign --monte-carlo --mode gtp --gtp-listen 1234
 let client = new GtpClient("localhost", 1234)
 client.BoardSize 9
 client.Komi 5.5
@@ -31,3 +32,15 @@ client.Move('W')
 client.Move('B')
 client.Board()
 client.Quit()
+
+open System
+open System.Windows.Forms
+ 
+[<EntryPoint>]
+[<STAThread>]
+let main argv = 
+    //Application.EnableVisualStyles()
+    //Application.SetCompatibleTextRenderingDefault false
+    use form = new Form()
+    Application.Run(form);
+    0 
